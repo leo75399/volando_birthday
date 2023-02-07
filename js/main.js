9b6181496d054b7eb1dc5ca36be66e793fa37a81
leo.liao <leo.liao@phimedia.tv>
2023-02-07 18:44:45 +0800

    update youtube function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,45 @@
-import nameList from "./nameList.json" assert { type: "json" };
+//修改人名
+let params = new URL(window.location.href).searchParams;
+let guestQuery = params.get("guest");
+changeName(guestQuery);
 
-console.log(nameList);
+function changeName(params) {
+  if (!!params) {
+    //如果有人名才執行
+    let guestName = jsondata.find((el) => {
+      return params === el.query;
+    });
+    document.querySelector(".alertBox_name").innerHTML = guestName.name;
+    document.querySelector(".content_name").innerHTML = guestName.name;
+  }
+}
 
+//載入 youtube
+const tag = document.createElement("script");
+tag.src = "https://www.youtube.com/iframe_api";
+// window.onYouTubeIframeAPIReady = loadVideo; // onYouTubeIframeAPIReady will load the video after the script is loaded
+const firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+let player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player("player", {
+    height: "360",
+    width: "640",
+    videoId: "md1tqfsKPOM",
+    playerVars: { controls: 0, loop: 1, fs: 0 },
+
+    events: {
+      onReady: onPlayerReady,
+    },
+  });
+}
+function onPlayerReady(event) {
+  event.target.mute();
+}
+
+//進場動態
+let alertBox = document.querySelector(".alertBox");
 let up = document.querySelector(".index_video");
 let down = document.querySelector(".index_content");
 let video = document.querySelector(".video");
@@ -10,13 +48,20 @@ let personName = document.querySelector(".content_name");
 let wish = document.querySelector(".content_wish");
 let happy = document.querySelector(".index_happy");
 
-up.addEventListener("animationend", (e) => {
+document.querySelector("#start").addEventListener("click", (e) => {
+  alertBox.classList.remove("alertBox_in");
+  up.classList.add("up_animation");
+  player.unMute(); //關閉靜音
+});
+
+up.addEventListener("animationstart", (e) => {
   if (e.animationName === "up_animation") {
     down.classList.add("down_animation");
   }
 });
 down.addEventListener("animationend", (e) => {
   video.classList.add("fade_animation");
+  player.playVideo(); //播放
 });
 video.addEventListener("animationend", (e) => {
   dear.classList.add("fade_animation");
---
+++ js/nameList.js
@@ -0,0 +1,14 @@
+let jsondata = [
+  {
+    name: "廖采揚",
+    query: "leo",
+  },
+  {
+    name: "賴麒宇",
+    query: "becker",
+  },
+  {
+    name: "羅智華",
+    query: "kevin",
+  },
+];
